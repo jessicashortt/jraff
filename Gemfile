9bd452d9ee567e3b8168181127b67b04b6618f5c
Jessica Shortt <me@jessicashortt.com>
2014-03-08 21:35:25 -0500

    Added a static pages controller

--- Gemfile
+++ Gemfile
@@ -4,7 +4,15 @@ source 'https://rubygems.org'
 gem 'rails', '4.0.3'
 
 # Use sqlite3 as the database for Active Record
-gem 'sqlite3'
+group :development, :test do
+  gem 'sqlite3'
+  # gem 'rspec-rails', '2.13.1'
+end
+
+group :test do
+  # gem 'selenium-webdriver', '2.35.1'
+  # gem 'capybara', '2.1.0'
+end
 
 # Use SCSS for stylesheets
 gem 'sass-rails', '~> 4.0.0'
--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,20 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-Jraff::Application.config.secret_key_base = 'bfa17693eb77f9b9a334a6b56c05299e07a4c6183f6dee36071f89b97297ca87d4d6ef19e37a8a2171901725a65120a27fde32651276ff01351ab3d2ce8a3bf4'
+
+require 'securerandom'
+
+def secure_token
+  token_file = Rails.root.join('.secret')
+  if File.exist?(token_file)
+    # Use the existing token.
+    File.read(token_file).chomp
+  else
+    # Generate a new token and store it in token_file.
+    token = SecureRandom.hex(64)
+    File.write(token_file, token)
+    token
+  end
+end
+
+Jraff::Application.config.secret_key_base = secure_token
