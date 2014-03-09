36f3c534ffbc44456b1bd96fc4f1e63c6d57dd7d
Jessica Shortt <me@jessicashortt.com>
2014-03-09 18:39:27 -0400

    User signup implemented.

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,5 +10,5 @@ class User < ActiveRecord::Base
   validates :username, presence: true, uniqueness: true
 
   has_secure_password
-  validates :password, length { minimum: 6 }
+  validates :password, length: { minimum: 6 }
 end
